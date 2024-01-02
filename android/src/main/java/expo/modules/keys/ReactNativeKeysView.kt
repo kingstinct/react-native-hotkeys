211c528da5a1fe2b68bb2803d79498d3b0d185c4
Robert Herber <robert@kingstinct.com>
2024-01-02 03:03:08 +0100

    Remove android

--- android/src/main/java/expo/modules/keys/ReactNativeKeysView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.keys
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class ReactNativeKeysView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
